6cc706476aed2168165aeacb6487952f360046a6
Artem Shaban <xtergs@gmail.com>
2019-08-07 22:38:31 +0300

    add styles to login page

--- CinemaCporeReactProject/clientapp1/src/Pages/Login.tsx
+++ CinemaCporeReactProject/clientapp1/src/Pages/Login.tsx
@@ -1,6 +1,9 @@
 ﻿import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Auth from '../auth';
+import LoginBackground from '../img/login_background.jpg';
+import Button from '@material-ui/core/Button';
+import { Input, TextField } from '@material-ui/core';
 
 
 class Login extends Component<any> {
@@ -25,12 +28,74 @@ class Login extends Component<any> {
         }
 
         return (
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                <p>You must log in to view the page</p>
-                <input type="email" />
-                <input type="password" />
-                <button onClick={this.login}>Log in</button>
+            <div>
+                <div style={{
+                    position: "absolute",
+                    zIndex: -1,
+                    display: "block",
+                    overflow: "hidden",
+                    minHeight: "100vh",
+                    height: "100%",
+                    width: "100%"
+                }}>
+                    <img src={LoginBackground} style={{
+                        minHeight: "100%",
+                        minWidth: "100%",
+                        filter: "brightness(50%)"
+                    }} />
+                </div>
+                <div
+                    style={{
+                        margin: "0 auto -236px",
+                        minHeight: "100vh",
+                        backgroundColor: "transparent",
+                        maxWidth: "450px",
+                        display: "block",
+                        color: "#333",
+                        padding: "0 5%"
+                    }}
+                >
+                    <div style={{
+                        padding: "60px 68px 40px",
+                        backgroundColor: "rgba(0,0,0,.75)",
+                        minHeight: "660px"
+                    }}>
+                        <div style={{ display: "flex", flexDirection: "column" }}>
+                            <h1
+                                style={{
+                                    color: "white",
+                                    fontSize: "32px",
+                                    fontWeight: 700,
+                                    marginBottom: "28px"
+                                }}
+                            >Sign In</h1>
+                            <TextField label="Email" required margin="normal" variant="filled"
+                                style={{
+                                    backgroundColor: "#333",
+                                    color: "#ffff",
+                                }}
+                                type="email"
+                            />
+                            <TextField label="Password" required margin="normal" variant="filled"
+                                style={{
+                                    backgroundColor: "#333",
+                                    color: "#ffff",
+                                }} type="password" />
+                            <div style={{height: "20px"}}/>
+                            <Button color="primary" onClick={this.login} variant="contained">Sign In</Button>
+
+                            <label>New to Netflix? <a href="#" onClick={(e) => {
+                                e.preventDefault();
+                                this.props.history.push('/register');
+                            }}
+                            style={{color: "white",
+                            textDecoration: "none"}}>Sign up now</a></label>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
         )
     }
 }
